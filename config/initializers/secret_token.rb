018c4966dbccc35d31ce9bfdd5f6ac4bc79d97b7
pradeepgupta <pradeepc2c@yahoo.com>
2013-01-13 11:39:13 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnopia::Application.config.secret_token = '84642702c6f7dd944e02419803e7629985c8eafc078518d1e4041f546a12b3f6cb8ff1e3b9ad23762f7258be6ea738f33a11dacd42fca98df5197e0b7d929caa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Learnopia::Application.config.session_store :cookie_store, key: '_learnopia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Learnopia::Application.config.session_store :active_record_store
